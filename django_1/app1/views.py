5cd196a2b63ffbadb008c6255278a3824a40d58f
Amir Razavi <s.amirhosein.rzv@gmail.com>
2025-02-11 10:02:18 -0800

    omadam va model ha kar kardam va data jadid onharo toye view ha nashon dadam

--- django_1/app1/views.py
+++ django_1/app1/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Todo
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    all = Todo.objects.all()
+    return render(request, "home.html", context={"all": all})
 
 def say_hello(request):
     person = {"name": "admin", "last_name": "Razavi"}
